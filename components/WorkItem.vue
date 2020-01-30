88dc705eb1ab45eeb16688b2c819d1f665574c5d
Per Lindström <per@digitaledge.se>
2020-01-30 19:59:42 +0100

    add work item component

---
+++ components/WorkItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="title">
+      {{ title }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/experience.vue
+++ pages/experience.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="container">
     <div>
-      <p>hello</p>
+      <work-item
+        v-for="exp in workExperiences"
+        :key="exp.title"
+        :title="exp.title"
+      />
       <a href="/">go back</a>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import WorkItem from '../components/WorkItem.vue'
 
+export default {
+  components: {
+    WorkItem
+  },
+  data () {
+    return {
+      workExperiences: [
+        {
+          title: 'Associate'
+        },
+        {
+          title: 'Research Intern'
+        },
+        {
+          title: 'Lead Software Engineer'
+        },
+        {
+          title: 'Software Engineer'
+        }
+      ]
+    }
+  }
 }
 </script>
 
