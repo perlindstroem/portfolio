ee23fcc3e11b0cc079571a8d02ecff24531651c5
Per Lindström <per@digitaledge.se>
2020-02-02 12:32:03 +0100

    implement mailing

--- functions/mail.js
+++ functions/mail.js
@@ -1,20 +1,48 @@
 import querystring from 'querystring'
 
-exports.handler = (event, context, callback) => {
+const mailjet = require('node-mailjet')
+  .connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
+
+exports.handler = async (event, context, callback) => {
   // Only allow POST
   if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' }
+    return { statusCode: 405, body: 'method not allowed' }
   }
 
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
   const params = querystring.parse(event.body)
-  const name = params.name || 'World'
 
-  console.log(name)
+  const name = params.name || 'none'
+  const email = params.email || 'none'
+  const message = params.message || 'none'
+
+  try {
+    await mailjet
+      .post('send', { 'version': 'v3.1' })
+      .request({
+        'Messages': [{
+          'From': {
+            'Email': 'per@lndstrm.se',
+            'Name': name
+          },
+          'To': [{
+            'Email': 'per@lndstrm.se',
+            'Name': 'Per Lindström'
+          }],
+          'Subject': `new form submission from portfolio [${name}/${email}]`,
+          'TextPart': message
+        }]
+      })
+  } catch (err) {
+    callback(null, {
+      statusCode: 422,
+      body: `something went wrong, ${err}`
+    })
+  }
 
   callback(null, {
     statusCode: 200,
-    body: `Hello, ${name}`
+    body: 'ok'
   })
 }
