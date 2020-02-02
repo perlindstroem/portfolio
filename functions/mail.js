ef99549612de31b7e682d8a9bd18d3c9b53f87b9
Per Lindström <per@digitaledge.se>
2020-02-02 12:40:29 +0100

    update mailing

--- functions/mail.js
+++ functions/mail.js
@@ -3,7 +3,7 @@ import querystring from 'querystring'
 const mailjet = require('node-mailjet')
   .connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
 
-exports.handler = async (event, context, callback) => {
+exports.handler = (event, context, callback) => {
   // Only allow POST
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'method not allowed' }
@@ -17,32 +17,31 @@ exports.handler = async (event, context, callback) => {
   const email = params.email || 'none'
   const message = params.message || 'none'
 
-  try {
-    await mailjet
-      .post('send', { 'version': 'v3.1' })
-      .request({
-        'Messages': [{
-          'From': {
-            'Email': 'per@lndstrm.se',
-            'Name': name
-          },
-          'To': [{
-            'Email': 'per@lndstrm.se',
-            'Name': 'Per Lindström'
-          }],
-          'Subject': `new form submission from portfolio [${name}/${email}]`,
-          'TextPart': message
-        }]
-      })
-  } catch (err) {
-    callback(null, {
-      statusCode: 422,
-      body: `something went wrong, ${err}`
+  mailjet
+    .post('send', { 'version': 'v3.1' })
+    .request({
+      'Messages': [{
+        'From': {
+          'Email': 'per@lndstrm.se',
+          'Name': name
+        },
+        'To': [{
+          'Email': 'per@lndstrm.se',
+          'Name': 'Per Lindström'
+        }],
+        'Subject': `new form submission from portfolio [${name}/${email}]`,
+        'TextPart': message
+      }]
     })
-  }
-
-  callback(null, {
-    statusCode: 200,
-    body: 'ok'
-  })
+    .then(() => {
+      callback(null, {
+        statusCode: 200,
+        body: 'ok'
+      })
+    }).catch((err) => {
+      callback(null, {
+        statusCode: 422,
+        body: `something went wrong, ${err}`
+      })
+    })
 }
