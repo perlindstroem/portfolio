771f2dee38a02a084ef75fe52a1ead2b736e75b4
Per Lindström <per@digitaledge.se>
2020-02-25 15:19:37 +0100

    move experiences to backend and add filtering

--- backend/app.js
+++ backend/app.js
@@ -79,4 +79,55 @@ async function sendMail ({ toMail, toName, fromMail, fromName, subject, message 
     })
 }
 
+app.get('/exp-data', (req, res) => {
+  const { filter } = req.query
+
+  const experience = [
+    {
+      title: 'Associate',
+      company: 'Digital Edge',
+      companyUrl: 'https://digitaledge.se',
+      start: '2019-09',
+      end: null,
+      description: [
+        'Working as a full stack or machine learning engineer depending on project assignment. Projects include; text classification and natural language processing, data aggregation  and visualisation, as well as a product planning tool for industrial manufacturing.'
+      ]
+    },
+    {
+      title: 'Research Intern',
+      company: 'Signality',
+      companyUrl: 'https://signality.com',
+      start: '2019-01',
+      end: '2019-07',
+      description: [
+        'Conducted research on imitation learning. Developed deep learning models for player behaviour modeling. Wrote a master\'s thesis and research paper based on the findings.'
+      ]
+    },
+    {
+      title: 'Lead Software Engineer',
+      company: 'Skira',
+      companyUrl: 'https://skira.se',
+      start: '2018-01',
+      end: '2019-02',
+      description: [
+        'Hired to help the founders build the technical platform for grain trading. Worked with the CTO to plan and implement the platform from being an idea to a live product.'
+      ]
+    },
+    {
+      title: 'Software Engineer',
+      company: 'GOLI',
+      companyUrl: 'https://goli.nu',
+      start: '2017-05',
+      end: '2017-12',
+      description: [
+        'Worked with the whole technical stack on a production and capacity planning application for the healthcare industry. Integrated several third party systems to feed production data into the application.'
+      ]
+    }
+  ]
+
+  const result = filter ? experience.filter(({ description }) => description[0].includes(filter)) : experience
+
+  res.status(200).send(result)
+})
+
 app.listen(port, '0.0.0.0', () => console.log(`server listening on port ${port}!`))
